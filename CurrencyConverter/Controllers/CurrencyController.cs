ff53466111d54774e20933c60f1eba3622dbeac2
Kavith <Kavith.com>
2025-10-20 17:57:28 +0530

    API created

---
+++ CurrencyConverter/Controllers/CurrencyController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private const string ApiKey = "tWTdo1sidkTTmXLMcVKcRiv3Sypq55Eb";
+
+        public CurrencyController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Add("apikey", ApiKey);
+        }
+
+        [HttpGet("symbols")]
+        public async Task<IActionResult> GetSymbols()
+        {
+            var url = "https://api.apilayer.com/exchangerates_data/symbols";
+            var response = await _httpClient.GetAsync(url);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+                return StatusCode((int)response.StatusCode, content);
+
+            return Content(content, "application/json");
+        }
+
+        [HttpGet("convert")]
+        public async Task<IActionResult> Convert( [FromQuery] string from,[FromQuery] string to,[FromQuery] decimal amount)
+        {
+            var url = $"https://api.apilayer.com/exchangerates_data/convert?from={from}&to={to}&amount={amount}";
+            var response = await _httpClient.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+                return StatusCode((int)response.StatusCode, content);
+
+            return Content(content, "application/json");
+        }
+    }
+}
--- CurrencyConverter/Program.cs
+++ CurrencyConverter/Program.cs
@@ -6,6 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+        policy.WithOrigins("http://localhost:4200")  // your Angular dev URL
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials());
+});
 
 var app = builder.Build();
 
@@ -15,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAngular");
 
 app.UseHttpsRedirection();
 
